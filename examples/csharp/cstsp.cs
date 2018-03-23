1094e6cbfe765fbcc1c932b119cfedfec6f7c6bf
Corentin Le Molgat <corentinl@google.com>
2018-03-23 13:51:55 +0100

    Example: cstsp Hack

--- examples/csharp/cstsp.cs
+++ examples/csharp/cstsp.cs
@@ -14,17 +14,26 @@
 using System;
 using System.Collections.Generic;
 using Google.OrTools.ConstraintSolver;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
 
-class Tsp
-{
+class Tsp {
+	internal class MainSearchMonitor : SearchMonitor {
+		public MainSearchMonitor(Solver s) : base(s) { }
+
+		public override bool AcceptDelta(Assignment delta, Assignment deltadelta)	{
+		  return base.AcceptDelta(delta, deltadelta);
+		}
+	}
+
   class RandomManhattan : NodeEvaluator2 {
-    public RandomManhattan(int size, int seed)
-    {
+    public RandomManhattan(int size, int seed) {
       this.xs_ = new int[size];
       this.ys_ = new int[size];
       Random generator = new Random(seed);
-      for (int i = 0; i < size; ++i)
-      {
+      for (int i = 0; i < size; ++i) {
         xs_[i] = generator.Next(1000);
         ys_[i] = generator.Next(1000);
       }
@@ -45,9 +54,10 @@ class Tsp
     }
   };
 
-  static void Solve(int size, int forbidden, int seed)
-  {
+  static void Solve(int size, int forbidden, int seed) {
     RoutingModel routing = new RoutingModel(size, 1, 0);
+		MainSearchMonitor monitor = new MainSearchMonitor(routing.solver());
+		routing.AddSearchMonitor(monitor);
 
     // Setting the cost function.
     // Put a permanent callback to the distance accessor here. The callback
@@ -84,24 +94,23 @@ class Tsp
         FirstSolutionStrategy.Types.Value.PathCheapestArc;
 
     Assignment solution = routing.SolveWithParameters(search_parameters);
-    Console.WriteLine("Status = {0}", routing.Status());
-    if (solution != null) {
-      // Solution cost.
-      Console.WriteLine("Cost = {0}", solution.ObjectiveValue());
-      // Inspect solution.
-      // Only one route here; otherwise iterate from 0 to routing.vehicles() - 1
-      int route_number = 0;
-      for (long node = routing.Start(route_number);
-           !routing.IsEnd(node);
-           node = solution.Value(routing.NextVar(node))) {
-        Console.Write("{0} -> ", node);
-      }
-      Console.WriteLine("0");
-    }
+    //Console.WriteLine("Status = {0}", routing.Status());
+    //if (solution != null) {
+    //  // Solution cost.
+    //  Console.WriteLine("Cost = {0}", solution.ObjectiveValue());
+    //  // Inspect solution.
+    //  // Only one route here; otherwise iterate from 0 to routing.vehicles() - 1
+    //  int route_number = 0;
+    //  for (long node = routing.Start(route_number);
+    //       !routing.IsEnd(node);
+    //       node = solution.Value(routing.NextVar(node))) {
+    //    Console.Write("{0} -> ", node);
+    //  }
+    //  Console.WriteLine("0");
+    //}
   }
 
-  public static void Main(String[] args)
-  {
+  public static void Main(String[] args) {
     int size = 10;
     if (args.Length > 0) {
       size = Convert.ToInt32(args[0]);
@@ -115,6 +124,22 @@ class Tsp
       seed = Convert.ToInt32(args[2]);
     }
 
-    Solve(size, forbidden, seed);
-  }
+		Console.WriteLine("ToTal Mem:             {0:N0}", GC.GetTotalMemory(true));
+		Solve(size, forbidden, seed);
+    Console.WriteLine("ToTal Mem (1 Iter):    {0:N0}", GC.GetTotalMemory(false));
+		GC.Collect();
+    Console.WriteLine("ToTal Mem (GC):        {0:N0}", GC.GetTotalMemory(true));
+
+		int n = 1;
+		const int N = 10;
+		while (true) {
+			n = 0;
+			while (n++ < N) {
+				Solve(size, forbidden, seed);
+			}
+			Console.WriteLine("ToTal Mem ({0} Iter): {1:N0}", N, GC.GetTotalMemory(false));
+			//GC.Collect();
+			//Console.WriteLine("ToTal Mem (GC):       {0:N0}", GC.GetTotalMemory(true));
+		}
+	}
 }

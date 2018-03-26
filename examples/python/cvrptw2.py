dd90dbb727bf8ca3b0826bc89adf249962fdc0e0
Corentin Le Molgat <corentinl@google.com>
2018-03-26 17:56:54 +0200

    Rework cvrptw.py
    Try to use possible data (e.g. currently, vehicles move at 36,000km/h...)

--- examples/python/cvrptw2.py
+++ examples/python/cvrptw2.py
@@ -1,17 +1,3 @@
-# This Python file uses the following encoding: utf-8
-# Copyright 2015 Tin Arm Engineering AB
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """Capacitated Vehicle Routing Problem with Time Windows (and optional orders).
 
    This is a sample using the routing library python wrapper to solve a
@@ -19,27 +5,11 @@
    A description of the problem can be found here:
    http://en.wikipedia.org/wiki/Vehicle_routing_problem.
    The variant which is tackled by this model includes a capacity dimension,
-   time windows and optional orders, with a penalty cost if orders are not
-   performed.
-   To help explore the problem, two classes are provided Customers() and
-   Vehicles(): used to randomly locate orders and depots, and to randomly
-   generate demands, time-window constraints and vehicles.
-   Distances are computed using the Great Circle distances. Distances are in km
-   and times in seconds.
-
-   A function for the displaying of the vehicle plan
-   display_vehicle_output
-
-   The optimization engine uses local search to improve solutions, first
-   solutions being generated using a cheapest addition heuristic.
-
+   and time windows performed.
+   Distances are in km and times in seconds.
 """
-
-import math
 from six.moves import xrange
 from ortools.constraint_solver import pywrapcp
-from ortools.constraint_solver import routing_enums_pb2
-
 
 def distance(x1, y1, x2, y2):
     # Manhattan distance
@@ -93,19 +63,29 @@ class CreateServiceTimeCallback(object):
   def ServiceTime(self, from_node, to_node):
     return self.matrix[from_node] * self.time_per_demand_unit
 
+# Create the travel time callback (equals distance divided by speed).
+class CreateTravelTimeCallback(object):
+  """Create callback to get travel times between locations."""
+
+  def __init__(self, dist_callback, speed):
+    self.dist_callback = dist_callback
+    self.speed = speed
+
+  def TravelTime(self, from_node, to_node):
+    travel_time = self.dist_callback(from_node, to_node) / self.speed
+    return int(travel_time)
 
 # Create total_time callback (equals service time plus travel time).
 class CreateTotalTimeCallback(object):
   """Create callback to get total times between locations."""
 
-  def __init__(self, service_time_callback, dist_callback, speed):
+  def __init__(self, service_time_callback, travel_time_callback):
     self.service_time_callback = service_time_callback
-    self.dist_callback = dist_callback
-    self.speed = speed
+    self.travel_time_callback = travel_time_callback
 
   def TotalTime(self, from_node, to_node):
     service_time = self.service_time_callback(from_node, to_node)
-    travel_time = self.dist_callback(from_node, to_node) / self.speed
+    travel_time = self.travel_time_callback(from_node, to_node)
     return service_time + travel_time
 
 def main():
@@ -114,6 +94,7 @@ def main():
   locations = data[0]
   demands = data[1]
   start_times = data[2]
+  end_times = data[3]
   num_locations = len(locations)
   depot = 0
   num_vehicles = 5
@@ -128,17 +109,7 @@ def main():
     routing = pywrapcp.RoutingModel(num_locations, num_vehicles, depot)
     search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
 
-    # Setting first solution heuristic: the
-    # method for finding a first solution to the problem.
-    search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-
-    # The 'PATH_CHEAPEST_ARC' method does the following:
-    # Starting from a route "start" node, connect it to the node which produces the
-    # cheapest route segment, then extend the route by iterating on the last
-    # node added to the route.
-
-    # Put callbacks to the distance function and travel time functions here.
+    # Callbacks to the distance function and travel time functions here.
     dist_between_locations = CreateDistanceCallback(locations)
     dist_callback = dist_between_locations.Distance
     routing.SetArcCostEvaluatorOfAllVehicles(dist_callback)
@@ -155,18 +126,20 @@ def main():
                          fix_start_cumul_to_zero, capacity)
 
     # Add time dimension constraints.
-    time_per_demand_unit = 300
+    # Time to deliver a package to a customer: ~3min/unit
+    time_per_demand_unit = 3 * 60
     horizon = 24 * 3600
     time = "Time"
-    speed = 10
-
-    # tw_duration is the width of the time windows.
-    tw_duration = 5 * 3600
+    # Travel speed: 50Km/h
+    speed = 50 / 3600
 
     service_times = CreateServiceTimeCallback(demands, time_per_demand_unit)
     service_time_callback = service_times.ServiceTime
 
-    total_times = CreateTotalTimeCallback(service_time_callback, dist_callback, speed)
+    travel_times = CreateTravelTimeCallback(dist_callback, speed)
+    travel_time_callback = travel_times.TravelTime
+
+    total_times = CreateTotalTimeCallback(service_time_callback, travel_time_callback)
     total_time_callback = total_times.TotalTime
 
     # Add a dimension for time-window constraints and limits on the start times and end times.
@@ -176,20 +149,17 @@ def main():
                          fix_start_cumul_to_zero,
                          time)
 
-    # Add limit on size of the time windows.
+    # Add time window constraints.
     time_dimension = routing.GetDimensionOrDie(time)
 
     for order in xrange(1, num_locations):
       start = start_times[order]
-      time_dimension.CumulVar(order).SetRange(start, start + tw_duration)
+      end = end_times[order]
+      time_dimension.CumulVar(order).SetRange(start, end)
 
     # Solve displays a solution if any.
     assignment = routing.SolveWithParameters(search_parameters)
     if assignment:
-      data = create_data_array()
-      locations = data[0]
-      demands = data[1]
-      start_times = data[2]
       size = len(locations)
       # Solution cost.
       print ("Total distance of all routes: " , str(assignment.ObjectiveValue()), '\n')
@@ -203,22 +173,26 @@ def main():
 
         while not routing.IsEnd(index):
           node_index = routing.IndexToNode(index)
+          location_index = locations[node_index]
           load_var = capacity_dimension.CumulVar(index)
           time_var = time_dimension.CumulVar(index)
           plan_output += \
-                    " {node_index} Load({load}) Time({tmin}, {tmax}) -> ".format(
+                    " {node_index}: Location{location_index} Load({load}) Time({tmin}, {tmax}) -> ".format(
                         node_index=node_index,
+                        location_index=location_index,
                         load=assignment.Value(load_var),
                         tmin=str(assignment.Min(time_var)),
                         tmax=str(assignment.Max(time_var)))
           index = assignment.Value(routing.NextVar(index))
 
         node_index = routing.IndexToNode(index)
+        location_index = locations[node_index]
         load_var = capacity_dimension.CumulVar(index)
         time_var = time_dimension.CumulVar(index)
         plan_output += \
-                  " {node_index} Load({load}) Time({tmin}, {tmax})".format(
+                " {node_index}: Location{location_index} Load({load}) Time({tmin}, {tmax})".format(
                       node_index=node_index,
+                      location_index=location_index,
                       load=assignment.Value(load_var),
                       tmin=str(assignment.Min(time_var)),
                       tmax=str(assignment.Max(time_var)))
@@ -230,21 +204,33 @@ def main():
 
 def create_data_array():
 
-  locations = [[82, 76], [96, 44], [50, 5], [49, 8], [13, 7], [29, 89], [58, 30], [84, 39],
-               [14, 24], [12, 39], [3, 82], [5, 10], [98, 52], [84, 25], [61, 59], [1, 65],
-               [88, 51], [91, 2], [19, 32], [93, 3], [50, 93], [98, 14], [5, 42], [42, 9],
-               [61, 62], [9, 97], [80, 55], [57, 69], [23, 15], [20, 70], [85, 60], [98, 5]]
+  locations = [[82, 76], [96, 44], [50,  5], [49,  8], [13,  7], [29, 89], [58, 30], [84, 39],
+               [14, 24], [12, 39], [ 3, 82], [ 5, 10], [98, 52], [84, 25], [61, 59], [ 1, 65],
+               [88, 51], [91,  2], [19, 32], [93,  3], [50, 93], [98, 14], [ 5, 42], [42,  9],
+               [61, 62], [ 9, 97], [80, 55], [57, 69], [23, 15], [20, 70], [85, 60], [98,  5]]
 
-  demands =  [0, 19, 21, 6, 19, 7, 12, 16, 6, 16, 8, 14, 21, 16, 3, 22, 18,
-             19, 1, 24, 8, 12, 4, 8, 24, 24, 2, 20, 15, 2, 14, 9]
+  demands =   [       0,       19,       21,        6,       19,        7,       12,       16,
+                      6,       16,        8,       14,       21,       16,        3,       22,
+                     18,       19,        1,       24,        8,       12,        4,        8,
+                     24,       24,        2,       20,       15,        2,       14,        9]
 
-  start_times =  [28842, 50891, 10351, 49370, 22553, 53131, 8908,
-                  56509, 54032, 10883, 60235, 46644, 35674, 30304,
-                  39950, 38297, 36273, 52108, 2333, 48986, 44552,
-                  31869, 38027, 5532, 57458, 51521, 11039, 31063,
-                  38781, 49169, 32833, 7392]
+  start_times = [     0,      508,      103,      493,      225,      531,       89,      565,
+                    540,      108,      602,      466,      356,      303,      399,      382,
+                    362,      521,       23,      489,      445,      318,      380,       55,
+                    574,      515,      110,      310,      387,      491,      328,       73]
 
-  data = [locations, demands, start_times]
+  # tw_duration is the width of the time windows (here 60min).
+  tw_duration = 10 * 60 * 60
+
+  # In this example, the width is the same at each location, so we define the end times to be
+  # start times + tw_duration. For problems in which the time window widths vary by location,
+  # you can explicitly define the list of end_times, as we have done for start_times.
+  end_times = [0] * len(start_times)
+
+  for i in range(len(start_times)):
+    end_times[i] = start_times[i] + tw_duration
+
+  data = [locations, demands, start_times, end_times]
   return data
 
 if __name__ == '__main__':
